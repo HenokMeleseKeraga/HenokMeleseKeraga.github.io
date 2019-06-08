6c2c4eae4eaf59ca39eaedaf134ea28e32f0da4c
Henok Melese <henmkeraga1@gmail.com>
2019-06-08 14:04:28 -0500

    ok

--- MouseMaze/maze.js
+++ MouseMaze/maze.js
@@ -4,18 +4,22 @@ $(function(){
     $(".boundary").mouseover(lose);
     $("#end").mouseover(win);
     $("#start").click(refresh);
+    
 });
 
-function lose(){
+function lose(e){
    
     $(".boundary").addClass('youlose');  
     $("#status").text("you loss");
+    $("#cor").text("(" +e.clientX+","+e.clientY +")");
+
    // alert("you loss");
    
 }
 function win(){
     $("#status").text("you win");
     //alert("you win");
+    $("#cor").text("(" +e.clientX+","+e.clientY +")");
 }
 function refresh(){
     $(".boundary").removeClass('youlose');
