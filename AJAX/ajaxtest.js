3c62ec64a1ee45b2192c6a8c398cd836896f8a7c
Henok Melese <henmkeraga1@gmail.com>
2019-06-10 17:39:26 -0500

    Update ajaxtest.js

--- AJAX/ajaxtest.js
+++ AJAX/ajaxtest.js
@@ -4,9 +4,16 @@
        url: 'https://github.com/HenokMeleseKeraga/HenokMeleseKeraga.github.io/blob/master/AJAX/bring.html'+$('#ppp').val(),
        success: ajaxSuccess,
        error: ajaxFailure
-    }
+    }   
 
   );
+  function ajaxSuccess(data) {
+	$('#txtajax"').val(data);
+}
+
+function ajaxFailure(xhr, status, exception) {
+  console.log(xhr, status, exception);
+}
 
 
 
